2a028d9685d77581d7a0db3693d63cda4f7b005f
Joaquin Elizondo <juacaelizondo@gmail.com>
2023-07-28 20:34:52 -0300

    comp

--- routes/songs/index.js
+++ routes/songs/index.js
@@ -1,17 +1,17 @@
-const songController = require('../../controllers/songController');
+const songController = require("../../controllers/songController");
 
-
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const cors = require('cors');
+const cors = require("cors");
 router.use(cors());
 
-router.post('/new', songController.create);
-router.get('/allsongs', songController.obtain);
-router.get('/:name', songController.obtainByName);
-router.get('/random/obtain', songController.obtainRandom)
+router.post("/new", songController.create);
+router.get("/allsongs", songController.obtain);
+router.get("/:name", songController.obtainByName);
+router.get("/random/obtain", songController.obtainRandom);
+router.get("/byGenre/obtain", songController.obtainbyGenre);
 
-router.delete('/:id', songController.delete);//deberia agregar un control aqui para que no cualquiera pueda eliminar canciones
+router.delete("/:id", songController.delete); //deberia agregar un control aqui para que no cualquiera pueda eliminar canciones
 
 module.exports = router;
